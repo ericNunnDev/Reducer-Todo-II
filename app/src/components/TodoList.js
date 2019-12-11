7009bd3754a987930133e6c37a9f442b87d50fe8
lowlyHero <e.nunn1009@gmail.com>
2019-12-11 14:22:48 -0500

    Cleaned up files. Added component and reducer folders and files.

---
+++ app/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TodoList = () => {
+    return ( 
+        <div>
+            <p>List</p>
+        </div>
+     );
+}
+ 
+export default TodoList
---
+++ app/src/reducers/index.js
@@ -0,0 +1,5 @@
+const initialState = {
+    id: 1,
+    item: 'Get my life together.',
+    completed: false
+}
